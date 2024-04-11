6d67dad71f9b82699d2ccd6f94dca2d08357248a
rainbowphysics <rainbowphysicsystem@gmail.com>
2024-04-11 15:15:07 +1000

    Tweak CI/CD again and remove scoping dependency

--- pytower/__init__.py
+++ pytower/__init__.py
@@ -1,12 +1,5 @@
 __version__ = '0.2.0'
 
-from scoping import scoping
+import os
 
-# Scope so that pytower doesn't always import os
-with scoping():
-    import os
-
-    root_directory = os.path.dirname(os.path.dirname(__file__))
-    scoping.keep('root_directory')
-
-del scoping
+root_directory = os.path.dirname(os.path.dirname(__file__))
