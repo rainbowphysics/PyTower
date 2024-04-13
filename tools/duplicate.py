2f0d72d9cef19713e44e53c716b2ae31d7d5b162
rainbowphysics <rainbowphysicsystem@gmail.com>
2024-04-13 10:18:19 +1000

    New tool: Duplicate

---
+++ tools/duplicate.py
@@ -0,0 +1,25 @@
+from pytower import tower
+from pytower.copy import copy_selection
+from pytower.selection import Selection
+from pytower.suitebro import Suitebro
+from pytower.tool_lib import ToolParameterInfo, ParameterDict
+from pytower.util import xyz
+from . import translate
+
+TOOL_NAME = 'Duplicate'
+VERSION = '1.0'
+AUTHOR = 'Physics System'
+URL = 'https://github.com/rainbowphysics/PyTower/blob/main/tools/duplicate.py'
+INFO = '''Duplicates selection (with optional offset)'''
+PARAMETERS = {'offset': ToolParameterInfo(dtype=xyz, description='Translation offset', default=xyz(0.0, 0.0, 0.0))}
+
+
+def main(save: Suitebro, selection: Selection, params: ParameterDict):
+    duplicated = copy_selection(selection)
+    save.add_objects(duplicated)
+
+    translate.main(save, duplicated, params)
+
+
+if __name__ == '__main__':
+    tower.run('CondoData', main, params=['offset=50,50,50'])
